64f088088589a588c23c4b017e9c8ad9c89a4969
aleksanderzych <rimmouane25@gmail.com>
2020-08-08 18:26:03 +0200

    create console ui

--- src/views/Album/PlayingConsole/PlayingConsole.jsx
+++ src/views/Album/PlayingConsole/PlayingConsole.jsx
@@ -4,10 +4,22 @@ import "./PlayingConsole.scss";
 const PlayingConsole = ({ previous, next }) => {
   return (
     <div className="playingConsole">
-      <button className="button" onClick={previous}>
+      <button
+        className="playingConsole__button playingConsole__button--previous"
+        onClick={previous}
+      >
         Previous
       </button>
-      <button className="button" onClick={next}>
+      <button
+        className="playingConsole__button playingConsole__button--play"
+        onClick={next}
+      >
+        play
+      </button>
+      <button
+        className="playingConsole__button playingConsole__button--next"
+        onClick={next}
+      >
         Next
       </button>
     </div>
