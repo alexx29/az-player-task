869d6d538f9d016dea1ae0754489dd271f47a702
aleksanderzych <rimmouane25@gmail.com>
2020-08-09 13:39:05 +0200

    init audio file

--- src/views/Album/Album.jsx
+++ src/views/Album/Album.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import MusicConsole from "./MusicConsole";
 import MusicSlider from "./MusicSlider";
+import Audio from "./Audio";
 
 const Album = () => (
   <>
     <MusicSlider />
     <MusicConsole />
+    <Audio />
   </>
 );
 
