2e541ffde61aa377dd673783aa725c88f2231f02
aleksanderzych <rimmouane25@gmail.com>
2020-08-09 14:24:38 +0200

    added funtionaliy audio

--- src/app/data.js
+++ src/app/data.js
@@ -3,18 +3,21 @@ export default [
     id: "1",
     name: "Self Conscious",
     author: "Kanye west",
+    duration: 150,
     img: require("../assets/images/unreleased_cover.png"),
   },
   {
     id: "2",
     name: "cos",
     author: "some",
+    duration: 230,
     img: require("../assets/images/cover-1.png"),
   },
   {
     id: "3",
     name: "feel the steel",
     author: "Steel Panther",
+    duration: 80,
     img: require("../assets/images/cover.png"),
   },
 ];
--- src/views/Album/Audio/Audio.jsx
+++ src/views/Album/Audio/Audio.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useMemo, useState, useEffect, useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { actuallyList, selectMusic, nextMusic } from "../musicsSlice";
+import getRandomMusic from "../../../helpers/getRandomMusic";
 
 const Audio = () => {
-  return <div>Audio test</div>;
+  const dispatch = useDispatch();
+  const {
+    repeat,
+    playing,
+    list,
+    selectedIndex,
+    selectedMusic,
+    shuffle,
+  } = useSelector(actuallyList);
+
+  const [lastSelectedIndex, setSelectedIndex] = useState(selectedIndex);
+  const [musicProgress, setMusicProgress] = useState(0);
+
+  const percent = useMemo(
+    () => (musicProgress / selectedMusic.duration) * 100,
+    [musicProgress, selectedMusic.duration]
+  );
+
+  const setCurrentMusic = useCallback(() => {
+    if (repeat) {
+      return dispatch(selectMusic(selectedIndex));
+    } else if (shuffle) {
+      return dispatch(selectMusic(getRandomMusic(selectedIndex, list.length)));
+    }
+    return dispatch(nextMusic());
+  }, [selectedIndex, shuffle, repeat, list]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (musicProgress < selectedMusic.duration) {
+        if (playing) {
+          return setMusicProgress(musicProgress + 10);
+        }
+      } else {
+        setCurrentMusic();
+        setMusicProgress(0);
+      }
+    }, 1000);
+
+    if (lastSelectedIndex !== selectedIndex) {
+      clearTimeout(timer);
+      setMusicProgress(0);
+      setSelectedIndex(selectedIndex);
+    }
+
+    return () => clearTimeout(timer);
+  }, [musicProgress, playing, selectedIndex]);
+
+  return (
+    <div>
+      <h1>{percent}</h1>
+    </div>
+  );
 };
 
 export default Audio;
